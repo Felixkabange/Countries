a03e02e68eff7f3825f45a787ed40473557385f0
Felly Kabange <felix@loglive.ai>
2025-10-21 23:25:49 -0500

    I have  implemented the API entry point and also setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home'
+import CountryDetails from './pages/CountryDetails'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="country/:code" element={<CountryDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
